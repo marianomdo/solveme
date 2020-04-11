7b905d9418ab23a105b5672caee28d7ea66648da
Alexander <alykoshin@gmail.com>
2020-04-11 04:53:04 +0300

    replace theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,25 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby Starter - Strata by HTML5 UP',
+    author: 'Hunter Chang',
+    description: 'A Gatsby.js Starter based on Strata by HTML5 UP',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
-
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline',
 
     /*'gatsby-source-buttercms', */{
       resolve: `gatsby-source-buttercms`,
@@ -45,10 +37,5 @@ module.exports = {
         //locales: [`en`, `es`, `fr`]
       },
     },
-    
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,56 +1,84 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
+import Helmet from 'react-helmet';
 import Layout from '../components/layout'
-import SEO from '../components/seo'
 
 const IndexPage = ({ data }) => {
   const home = data.home.edges[0].node
 
   return (
     <Layout>
-      <SEO
-        title={home.seo_title}
-      />
+      <Helmet>
+        <title>{home.seo_title}</title>
+        <meta name="description" content={home.seo_title} />
+      </Helmet>
 
-      <div>
-        <h1>{home.headline}</h1>
-        <img src={home.hero_image} />
-          <button>{home.call_to_action}</button>
-      </div>
+      <div id="main">
+        <section id="one">
+          <header className="major">
 
-      <h1>Our Customers</h1>
-      <div>
-        {home.customer_logos.map(({ logo_image }) => (
-          <img
-            key={logo_image}
-            src={logo_image}
-          />
-        ))}
+            <h1>{home.headline}</h1>
+          </header>
+          <img src={home.hero_image} />
+
+          <a href="#" className="button">
+            {home.call_to_action}
+          </a>
+
+        </section>
+
+        <section id="one">
+          <h1>Our Customers</h1>
+          <div>
+            {home.customer_logos.map(({ logo_image }) => (
+
+              <div>
+                <article
+                  className="6u 12u$(xsmall)"
+                  key={logo_image}
+                >
+                  <a
+                    className="image fit thumb"
+                    href="#"
+                    onClick={e => {}}
+                  >
+                    <img
+                      src={logo_image}
+                    />
+                  </a>
+                  <h3>Caption</h3>
+                  <p>Description</p>
+                </article>
+
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
 
     </Layout>
-)
+  )
 
 }
 
 //GraphQl query to fetch homepage data
 export const query = graphql`
-{
-  home: allButterPage(filter: { slug: { eq: "homepage" } }) {
-  edges {
-  node {
-  slug
-  headline
-  seo_title
-  customer_logos {
-  logo_image
-}
-  hero_image
-  call_to_action
-}
-}
-}
-}
+  {
+    home: allButterPage(filter: { slug: { eq: "homepage" } }) {
+      edges {
+        node {
+          slug
+          headline
+          seo_title
+          customer_logos {
+            logo_image
+          }
+          hero_image
+          call_to_action
+        }
+      }
+    }
+  }
 `
 
 export default IndexPage
